8cc64ea896653d81c2b1518627d058f061e4c4cc
Lizandro <lizandrojesus13@hotmail.com>
2024-05-05 01:26:12 +0200

    Feat: Añadiendo component para ver los productos cuando el usuario esté autenticado

--- src/app/routes/RoutePrivate.jsx
+++ src/app/routes/RoutePrivate.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Profile } from '../auth/Profile/Profile'
 import { Main } from '../components/main/Main'
+import { ProductsList } from '../products/ProductsList'
 
 export const RoutePrivate = () => {
   return (
     <Routes>
-      <Route path='/profile' element={<Profile />} />
+      <Route exact path='/profile/*' element={<Profile />} />
+      <Route exact path='/products' element={<ProductsList />} />
       <Route exact path='/' element={<Main />} />
     </Routes>
   )
