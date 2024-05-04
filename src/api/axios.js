b296abebb964dc528155d96b89cc5287bfae45d5
Lizandro <lizandrojesus13@hotmail.com>
2024-05-05 01:18:05 +0200

    Feat: Actualizando la obtencion y envio del token

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { GetEnviroments } from "../helpers/GetEnviroments";
-const { VITE_BASE_URL } = GetEnviroments();
 
+const { VITE_BASE_URL } = GetEnviroments();
 export const springBootAxios = axios.create({ baseURL: VITE_BASE_URL });
-const Authorization = `Bearer ${localStorage.getItem("token")}`
+
+const token = localStorage.getItem("token");
 springBootAxios.interceptors.request.use(config => {
     config.headers = {
         ...config.headers,
-        Authorization
+        Authorization: token ? `Bearer ${token}` : null
     };
-
+    
     return config;
 });
