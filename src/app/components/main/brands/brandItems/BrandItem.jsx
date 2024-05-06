80d4d2fae63ecfac3185b1cc049d9330365171a6
Lizandro <lizandrojesus13@hotmail.com>
2024-05-06 23:57:19 +0200

    Fix: Solucionando enlaces imagenes

--- src/app/components/main/brands/brandItems/BrandItem.jsx
+++ src/app/components/main/brands/brandItems/BrandItem.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
 import "./BrandItem.css";
+import BrandImg from "../../../../../../public/brands/brands.jpg"
 export const BrandItem = () => {
     return (
         <div className='grid-brand-item'>
             <div>
-                <img src="../../../../public/brands/brands.jpg" alt="" />
+                <img src={BrandImg} alt="" />
             </div>
             <div>
-                <img src="../../../../public/brands/brands.jpg" alt="" />
+                <img src={BrandImg} alt="" />
             </div>
             <div>
-                <img src="../../../../public/brands/brands.jpg" alt="" />
+                <img src={BrandImg} alt="" />
             </div>
             <div>
-                <img src="../../../../public/brands/brands.jpg" alt="" />
+                <img src={BrandImg} alt="" />
             </div>
             <div>
-                <img src="../../../../public/brands/brands.jpg" alt="" />
+                <img src={BrandImg} alt="" />
             </div>
             <div>
-                <img src="../../../../public/brands/brands.jpg" alt="" />
+                <img src={BrandImg} alt="" />
             </div>
             <div>
-                <img src="../../../../public/brands/brands.jpg" alt="" />
+                <img src={BrandImg} alt="" />
             </div>
             <div>
-                <img src="../../../../public/brands/brands.jpg" alt="" />
+                <img src={BrandImg} alt="" />
             </div>
         </div>
     )
