c1f00e919b6f031e6e0f3b142f5395ba9bc7eaf5
Lizandro <lizandrojesus13@hotmail.com>
2024-06-04 23:50:22 +0200

    Feat: Spinner de carga de categorias y productos

---
+++ src/app/products/Spinner/SpinnerCategory.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./SpinnerCategory.css"
+export const SpinnerCategory = () => {
+    return (
+        <span class="loader-category"></span>
+    )
+}
---
+++ src/app/products/Spinner/SpinnerProducts.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import "./SpinnerProducts.css";
+export const SpinnerProducts = () => {
+    return (
+        <span class="loader"></span>
+    )
+}
