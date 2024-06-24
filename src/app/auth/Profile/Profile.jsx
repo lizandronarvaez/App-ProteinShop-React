86b3912de181025e473aa062029d8dea967ef3cf
Lizandro <lizandrojesus13@hotmail.com>
2024-06-24 19:55:25 +0200

    commit

--- src/app/auth/Profile/Profile.jsx
+++ src/app/auth/Profile/Profile.jsx
@@ -2,38 +2,39 @@ import React, { useContext, useEffect } from 'react'
 import "./Profile.css";
 import userProfile from "../../../../public/svg/userProfile.svg"
 import userOrders from "../../../../public/svg/userOrders.svg"
-import { Link, Route, Routes, useLocation } from 'react-router-dom';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import { ProfileForm } from './ProfileForm';
 import { OrdersUsers } from '../Orders/OrdersUsers';
 import { AuthContext } from '../context/authContext';
 
 export const Profile = () => {
-    const { pathname } = useLocation();
-    // todo:Obetener los datos del cliente desde el local storage
     const user = JSON.parse(localStorage.getItem("cliente"));
-    // TODO: ALMACENAR LOS DATOS EN UN OBJETO
-
-    // todo: enviar peticion al backend para actualizar los datos
-
+    const { logged } = useContext(AuthContext);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!logged) {
+            navigate("/account")
+        }
+    }, [logged, navigate, user])
+    if (!logged) return null;
 
     return (
-
-            <div className='profile-user'>
-                <h2>Perfil Usuario</h2>
-                <div className='profile-user-grid'>
-                    <div className='profile-user-nav'>
-                        <ul>
-                            <Link to="/profile/user"><li><img src={userProfile} alt={userProfile} />Datos Personales</li></Link>
-                            <Link to="/profile/orders_users"><li><img src={userOrders} alt={userProfile} />Pedidos</li></Link>
-                        </ul>
-                    </div>
-                    <div className='profile-user-data'>
-                        <Routes>
-                            <Route path="/user" element={<ProfileForm user={user} />} />
-                            <Route path="/orders_users" element={<OrdersUsers />} />
-                        </Routes>
-                    </div>
+        <div className='profile-user' >
+            <h2>Perfil Usuario</h2>
+            <div className='profile-user-grid'>
+                <div className='profile-user-nav'>
+                    <ul>
+                        <Link to="/profile/user"><li><img src={userProfile} alt={userProfile} />Datos Personales</li></Link>
+                        <Link to="/profile/orders_users"><li><img src={userOrders} alt={userProfile} />Pedidos</li></Link>
+                    </ul>
+                </div>
+                <div className='profile-user-data'>
+                    <Routes>
+                        <Route path="/user" element={<ProfileForm user={user} />} />
+                        <Route path="/orders_users" element={<OrdersUsers />} />
+                    </Routes>
                 </div>
             </div>
+        </div >
     )
 }
