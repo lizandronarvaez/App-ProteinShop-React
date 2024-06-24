65b323ea0845d7a90664b9291494b9fb5a45687c
Lizandro <lizandrojesus13@hotmail.com>
2024-06-24 19:52:20 +0200

    Feat: Carrito con ventana desplegable

--- src/app/components/Trolley/Trolley.jsx
+++ src/app/components/Trolley/Trolley.jsx
@@ -5,22 +5,25 @@ import cartout from "../../../../public/svg/cart_out.svg";
 import { useCart } from '../../context/CartTrolleyContext';
 export const Trolley = ({ trolleyIsOpen, setTrolleyIsOpen }) => {
   const navigate = useNavigate();
+
   const { cartProducts, setCartProducts } = useCart();
-  const [totalProductsCart, setTotalProductsCart] = useState(0);
+  // const [totalProductsCart, setTotalProductsCart] = useState(0);
 
   const closeDivCart = () => setTrolleyIsOpen(false);
-  // Eliminar productos del carrito
   const deleteProductCartList = (id) => {
     const filterProductList = cartProducts.filter(product => product.id !== id);
     setCartProducts(filterProductList);
   }
-  // Calculo de los productos del carrito
-  const totalProducts = cartProducts.reduce((a, b) => { return a + b.price }, 0);
-  useEffect(() => { }, [cartProducts])
 
+  const totalProducts = cartProducts.reduce((a, b) => { return a + b.price }, 0);
+  
   const makeOrder = () => {
+    localStorage.setItem("cart", JSON.stringify(cartProducts))
     navigate("/submit-order")
   }
+  
+  useEffect(() => { }, [cartProducts])
+  
   return (
     <div className={trolleyIsOpen ? "trolley" : "trolley-remove"}>
       {/* Titulo */}
@@ -31,12 +34,14 @@ export const Trolley = ({ trolleyIsOpen, setTrolleyIsOpen }) => {
       {/* Fin titulo */}
       {
         cartProducts.map((product, i) => (
+          // todo:solucionar inputs para recogida de informacion de uds
           <div className='trolley_products' key={i}>
             <div className='trolley_product_item'>
               <h3>{product.fullname}</h3>
               <p className='product_img_price'>
-                <img className='img_cart' src={product.imageProduct} alt="" />
+                <img className='img_cart' src={product.imageProduct} alt={product.imageProduct} />
                 {product.price}€
+                {/* <input type="number" name="quantity" defaultValue={1} min={1} /> */}
               </p>
             </div>
             <p onClick={() => deleteProductCartList(product.id)}>x</p>
@@ -56,15 +61,15 @@ export const Trolley = ({ trolleyIsOpen, setTrolleyIsOpen }) => {
             <div className='trolley_total'>
               <div>
                 <p>Envio:</p>
-                {totalProductsCart > 30 ? <p>Gratuito</p> : <p>5,99€</p>}
+                {totalProducts.toFixed(2) > 30 ? <p>Gratuito</p> : <p>5,99€</p>}
               </div>
               <div>
                 <p>Total</p>
                 {
-                  totalProductsCart < 30 && totalProductsCart
+                  totalProducts.toFixed(2) < 30 && totalProducts
                     ?
                     (
-                      <p>{(totalProductsCart + 5.99).toFixed(2)}€</p>
+                      <p>{(totalProducts + 5.99).toFixed(2)}€</p>
                     )
                     : <p>{totalProducts.toFixed(2)}€</p>
                 }
