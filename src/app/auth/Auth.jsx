d1a9f727cdbcc8b0c604eb017c648b832913fbf5
Lizandro <lizandrojesus13@hotmail.com>
2024-06-24 19:49:34 +0200

    Feat: Agregando useEffect para mostrar o no el componente cuando se haya logeado el usuario

--- src/app/auth/Auth.jsx
+++ src/app/auth/Auth.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Register } from './Register/Register'
 import { Login } from './Login/Login'
 import './Auth.css'
+import { AuthContext } from './context/authContext'
 export const Auth = () => {
+    const { logged } = useContext(AuthContext);
+    useEffect(() => { }, [logged])
     return (
-        <div className='container auth-form'>
-            <h2>Acceso a tu cuenta</h2>
-            <div className='login-register'>
-                <Login />
-                <Register />
-            </div>
-        </div>
+        !logged ?
+            (
+                <div className='container auth-form'>
+                    <h1>Acceso a tu cuenta</h1>
+                    <p>Por favor complete la siguiente información</p>
+                    <div className='login-register'>
+                        <Login />
+                        <Register />
+                    </div>
+                </div>
+            )
+            : null
     )
 }
