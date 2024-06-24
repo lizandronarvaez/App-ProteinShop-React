472644baaec54c22ef29ee76cff1d58da11b30b8
Lizandro <lizandrojesus13@hotmail.com>
2024-06-24 19:56:23 +0200

    Feat: Realizando componente para consultar los pedidos del cliente

--- src/app/auth/Orders/OrdersUsers.jsx
+++ src/app/auth/Orders/OrdersUsers.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import "./OrdersUsers.css";
 
 export const OrdersUsers = () => {
   return (
-    <div>Componente de pedidos de usuarios</div>
+    <div className='order-users'>
+      <div>
+        <h2>Pedido 1</h2>
+        <p>Info Pedido</p>
+      </div>
+      <div>
+        <h2>Pedido 1</h2>
+        <p>Info Pedido</p>
+      </div>
+      <div>
+        <h2>Pedido 1</h2>
+        <p>Info Pedido</p>
+      </div>
+      <div>
+        <h2>Pedido 1</h2>
+        <p>Info Pedido</p>
+      </div>
+
+    </div>
   )
 }
