a8d9cc2871846a7c37b82d7eb696dfa1352b49d6
Lizandro <lizandrojesus13@hotmail.com>
2024-06-24 19:51:04 +0200

    Feat: Eliminar exact ya que la version 6 de react-router no hace falta

--- src/app/routes/RoutePrivate.jsx
+++ src/app/routes/RoutePrivate.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { Profile } from '../auth/Profile/Profile'
 import { Main } from '../components/main/Main'
 import { ProductsList } from '../products/ProductsList'
@@ -8,11 +8,12 @@ import { OrderProducts } from '../order/OrderProducts'
 export const RoutePrivate = () => {
   return (
     <Routes>
-      <Route exact path='/profile/*' element={<Profile />} />
+      <Route path='/profile/*' element={<Profile />} />
       
-      <Route exact path='/submit-order' element={<OrderProducts />} />
-      <Route exact path='/products' element={<ProductsList />} />
-      <Route exact path='/' element={<Main />} />
+      <Route path='/submit-order' element={<OrderProducts />} />
+      <Route path='/products' element={<ProductsList />} />
+
+      <Route path='/' element={<Main />} />
     </Routes>
   )
 }
--- src/app/routes/RoutePublic.jsx
+++ src/app/routes/RoutePublic.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { ProductsList } from '../products/ProductsList'
 import { Login } from '../auth/Login/Login'
 import { Main } from '../components/main/Main'
@@ -9,13 +9,12 @@ import { OrderProducts } from '../order/OrderProducts';
 export const RoutePublic = ({ elements }) => {
   return (
     <Routes>
-      <Route exact path='/products' element={<ProductsList />} />
-      <Route exact path='/submit-order' element={<OrderProducts />} />
-      <Route exact path='/account' element={<Auth />} />
-      <Route exact path='/account/form-register' element={<RegisterForm />} />
+      <Route path='/products' element={<ProductsList />} />
+      <Route path='/submit-order' element={<OrderProducts />} />
+      <Route path='/account' element={<Auth />} />
+      <Route path='/account/form-register' element={<RegisterForm />} />
 
-      <Route exact path='/' element={<Main />} />
-
+      <Route path='/' element={<Main />} />
     </Routes>
   )
 }
