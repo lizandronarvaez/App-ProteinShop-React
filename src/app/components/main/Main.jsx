c50e032c7f78c2de30129c8b778d95a34fd0d3b9
Lizandro <lizandrojesus13@hotmail.com>
2024-07-06 15:44:19 +0200

    Feat: Solucionando componente que no redenrizaba

--- src/app/components/main/Main.jsx
+++ src/app/components/main/Main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import "./Main.css";
 import { Top_Sales } from './top-sales/Top_Sales';
 import { Discounts } from './discounts/Discounts';
-import { Brands } from './brands/Brands';
 import { InfoStore } from './info-store/InfoStore';
 export const Main = () => {
   return (
