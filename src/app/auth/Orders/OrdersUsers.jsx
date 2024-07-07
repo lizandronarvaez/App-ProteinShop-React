def02670f80b84720cd3f3fca2cacf9102db2b5d
Lizandro <lizandrojesus13@hotmail.com>
2024-07-07 03:35:52 +0200

    Pendiente de realizar logica para obtener los pedidos de cada usuario

--- src/app/auth/Orders/OrdersUsers.jsx
+++ src/app/auth/Orders/OrdersUsers.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./OrdersUsers.css";
+import { springBootAxios } from '../../../api/axios';
 
 export const OrdersUsers = () => {
+  const { id } = JSON.parse(localStorage.getItem("cliente"));
+
+  // TODO: REALIZAR CONSULTA PARA VER LOS PEDIDOS DEL CLIENTE!
+  const getOrderByUserId = async () => {
+    const getOrderDB = await springBootAxios.get(`/orders`);
+    console.log(getOrderDB)
+  }
+
+  useEffect(() => {
+    getOrderByUserId();
+  }, []);
   return (
     <div className='order-users'>
       <div>
