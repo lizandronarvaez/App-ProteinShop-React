4464dfeffd5754008d3fd7b9aa293ff1fe589afd
Lizandro <lizandrojesus13@hotmail.com>
2024-07-07 05:32:48 +0200

    Feat: Menu hamburgesa para los enlaces de la aplicacion

--- src/app/components/nav/NavItems/NavItems.jsx
+++ src/app/components/nav/NavItems/NavItems.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom';
 import "./NavItems.css";
+import Menu from "../../../../../public/svg/menu.svg"
 export const NavItems = () => {
+  const [toggle, setToggle] = useState(true);
+  const myRef = useRef();
+
+  // TODO!!SOLUCIONAR EL TEMA DE MENU HAMBURGER PARA LOS ENLACES
+  const toggleClass = () => {
+    if (window.innerWidth < 768) {
+      setToggle(!toggle)
+    }else{
+      setToggle(false)
+    }
+  }
+
+  useEffect(() => {
+    toggleClass();
+  }, [])
+  console.log(toggle)
   return (
-    <nav className='nav-items'>
-      <ul>
-        <li><Link to="/products">Productos</Link></li>
-        <li>Accesorios</li>
-        <li>Top Ventas</li>
-        <li>Promociones</li>
-      </ul>
-    </nav>
+    <>
+      <nav className='nav-items'>
+        <div className='hamburger-menu'>
+          <img src={Menu} alt={Menu} onClick={toggleClass} />
+        </div>
+        <div className='items-menu'>
+          {
+            toggle ? (
+              <ul>
+                <li><Link to="/products" onClick={() => setToggle(false)}>Productos</Link></li>
+                <li>Accesorios</li>
+                <li>Top Ventas</li>
+                <li>Promociones</li>
+              </ul>
+            ) : null
+          }
+
+        </div>
+      </nav>
+    </>
   )
 }
