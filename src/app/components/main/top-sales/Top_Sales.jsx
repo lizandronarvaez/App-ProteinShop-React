f5abbda360122ee29830b21393e95894c090aede
Lizandro <lizandrojesus13@hotmail.com>
2024-07-07 03:29:02 +0200

    Logica para componente de topventas

--- src/app/components/main/top-sales/Top_Sales.jsx
+++ src/app/components/main/top-sales/Top_Sales.jsx
@@ -3,13 +3,26 @@ import "./Top_Sales.css";
 import { Grid_Card_Item } from './Grid-Card/Grid_Card_Item';
 import { springBootAxios } from '../../../../api/axios';
 import { SpinnerProducts } from '../../../products/Spinner/SpinnerProducts';
-export const Top_Sales = () => {
-    const [products, setProducts] = useState([]);
+import { useCart } from '../../../context/CartTrolleyContext';
+import Swal from "sweetalert2/dist/sweetalert2.all";
 
+export const Top_Sales = () => {
+    const [productsDB, setProductsDB] = useState([]);
+    const {cartProducts,setCartProducts}=useCart();
     const getDataDB = async () => {
         const { data } = await springBootAxios.get("/products")
         const filterProductsTop = data.filter(product => product.price >= 10 && product.price <= 50)
-        setProducts(filterProductsTop)
+        setProductsDB(filterProductsTop)
+    }
+
+    const addProductCartList=(product)=>{
+        const isProductInCart = cartProducts.some(item => item.id === product.id);
+        if (isProductInCart) {
+            Swal.fire("El producto ya está en tu carrito", "", "error")
+            return;
+        }
+        Swal.fire("Agregado a carrito", "", "success")
+        setCartProducts([...cartProducts, product])
     }
     useEffect(() => { getDataDB(); }, [])
     return (
@@ -17,7 +30,7 @@ export const Top_Sales = () => {
             <h2>TOP Productos Ventas</h2>
             <div className='grid'>
                 {
-                    !products.length ?
+                    !productsDB.length ?
                         (
                             <>
                                 <SpinnerProducts />
@@ -31,7 +44,7 @@ export const Top_Sales = () => {
 
                             </>
                         )
-                        : <Grid_Card_Item productsTop={products} />
+                        : <Grid_Card_Item productsTop={productsDB} addProductCartList={addProductCartList} />
 
                 }
             </div>
