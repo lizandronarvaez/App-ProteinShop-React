d3c23f78da7377fc1d3b9b6a5ea1dcffd11b777a
Lizandro <lizandrojesus13@hotmail.com>
2024-07-07 04:02:38 +0200

    Logica para el componente de suscripciones de promociones para usuario

--- src/app/components/main/discounts/FormDiscounts/FormDiscounts.jsx
+++ src/app/components/main/discounts/FormDiscounts/FormDiscounts.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./FormDiscounts.css";
+
+const form = {
+    suscription: ""
+}
 export const FormDiscounts = () => {
+    const [dataForm, setDataForm] = useState(form);
+
+    const inputValueForm = ({ target: { name, value } }) => setDataForm({...dataForm,[name]: value});
+    
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+
+        // todo:realizar logica para registrar cliente suscripto a promociones
+        console.log(dataForm)
+    }
     return (
         <div className='discounts-form'>
-            <form>
+            <form onSubmit={onSubmitForm}>
                 <div className='discounts-form-box'>
                     <label htmlFor="suscription"></label>
-                    <input type="text" name="suscription" id='suscription' placeholder='Introduce tu email' />
+                    <input type="text" onChange={inputValueForm} name="suscription" id='suscription' placeholder='Introduce tu email' />
                 </div>
                 <div className='discounts-form-box discounts-form-box_submit'>
                     <button type="submit">Suscribirse</button>
