3a5d24e7724be66e556bb6b4f5414d046183ad79
Lizandro <lizandrojesus13@hotmail.com>
2024-07-08 23:33:10 +0200

    Feat: Realizando ajustes y añadiendo por quien ha sido desarrollado

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./Footer.css";
 import { Link } from 'react-router-dom';
+import { AuthContext } from "../../auth/context/authContext";
 
 export const Footer = () => {
+  const { logged } = useContext(AuthContext);
+
   return (
     <div className='footer-container'>
       <div className='footer'>
         <div className='footer-box'>
           <h3>Contacto</h3>
           <div>
-            <p>Atención cliente: +34 92612341234</p>
-            <p>Contestamos a las consultas en un máximo de 48 horas.</p>
+            <ul>
+              <li>Atención cliente: <b> +34 92612341234</b></li>
+              <li>Email: <b>contacto@proteinshop.com</b></li>
+              <li>Contestamos a las consultas en un máximo de 48 horas.</li>
+            </ul>
           </div>
         </div>
         <div className='footer-box'>
@@ -29,14 +35,29 @@ export const Footer = () => {
           <h3>Usuario</h3>
           <div>
             <ul>
-              <li><Link to={"/account"}>Mi cuenta </Link> </li>
-              <li><Link to={"/account"}> Historial de pedidos</Link> </li>
+              <li>
+                {
+                  !logged
+                    ? <Link to={"/account"}>Mi cuenta </Link>
+                    : <Link to={"/profile/orders_users"}>Mi cuenta </Link>
+
+                }
+              </li>
+              <li>
+                {
+                  !logged ?
+                    <Link to={"/account"}> Historial de pedidos</Link>
+                    : <Link to={"/profile/user"}>Historial de pedidos </Link>
+                }
+              </li>
               <li>Recuperar contraseña</li>
             </ul>
           </div>
         </div>
       </div>
-
+      <div className='user-project'>
+        <p><b>Desarrollado por Lizandro Narváez</b> </p>
+      </div>
     </div >
   )
 }
