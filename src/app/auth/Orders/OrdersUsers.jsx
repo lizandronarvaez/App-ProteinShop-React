eb60d0e3a1b804997e079b03c45d7985e941364c
Lizandro <lizandrojesus13@hotmail.com>
2024-07-11 23:08:52 +0200

    Feat: Obteniendo los pedidos del cliente y conocer el historial

--- src/app/auth/Orders/OrdersUsers.jsx
+++ src/app/auth/Orders/OrdersUsers.jsx
@@ -1,38 +1,35 @@
 import React, { useEffect } from 'react';
 import "./OrdersUsers.css";
 import { springBootAxios } from '../../../api/axios';
-
+import { useState } from 'react';
+import Pdf from "../../../../public/svg/pdf.svg";
 export const OrdersUsers = () => {
   const { id } = JSON.parse(localStorage.getItem("cliente"));
-
+  const [ordersClientDb, setOrdersClientDb] = useState([]);
   // TODO: REALIZAR CONSULTA PARA VER LOS PEDIDOS DEL CLIENTE!
   const getOrderByUserId = async () => {
-    const getOrderDB = await springBootAxios.get(`/orders`);
-    console.log(getOrderDB)
+      const { data: { orderList } } = await springBootAxios.get(`/orders/${id}`);
+      setOrdersClientDb(orderList)
   }
-
-  useEffect(() => {
-    getOrderByUserId();
-  }, []);
+  const orderViewPdf = () => {
+    // TODO: REALIZAR PDF PARA VISIONAR EL PDF CON EL PEDIDO DEL CLIENTE
+    window.open("", "_blank")
+  }
+  useEffect(() => { getOrderByUserId(); }, [id]);
   return (
     <div className='order-users'>
-      <div>
-        <h2>Pedido 1</h2>
-        <p>Info Pedido</p>
-      </div>
-      <div>
-        <h2>Pedido 1</h2>
-        <p>Info Pedido</p>
-      </div>
-      <div>
-        <h2>Pedido 1</h2>
-        <p>Info Pedido</p>
-      </div>
-      <div>
-        <h2>Pedido 1</h2>
-        <p>Info Pedido</p>
-      </div>
-
+      {
+        ordersClientDb.map((order, i) => (
+          <div key={order.id}>
+            <ul>
+              <li>Nº Pedido: <span>{order.id}</span></li>
+              <li>Fecha Pedido: <span>{order.createdAt}</span> </li>
+              <li>Total Pedido: <span>{order.total}€</span> </li>
+              <li>Ver Pedido:<img src={Pdf} alt={Pdf} onClick={orderViewPdf} /></li>
+            </ul>
+          </div>
+        ))
+      }
     </div>
   )
 }
