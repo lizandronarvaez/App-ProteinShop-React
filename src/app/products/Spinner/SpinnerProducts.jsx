48a27d9d65571f2feb4ab1cc5b0f4b3864cb9adf
Lizandro <lizandrojesus13@hotmail.com>
2024-07-11 23:08:16 +0200

    Cambio de nombre al spinner de carga del componente producto

--- src/app/products/Spinner/SpinnerProducts.jsx
+++ src/app/products/Spinner/SpinnerProducts.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import "./SpinnerProducts.css";
 export const SpinnerProducts = () => {
     return (
-        <span className="loader"></span>
+        <span className="loader-products"></span>
     )
 }
