0168fc9d8df89e782efcd2dc4260aad2b6734cd1
Lizandro <lizandrojesus13@hotmail.com>
2024-07-28 20:01:27 +0200

    Archivo barril para iconos svg

---
+++ public/index.js
@@ -0,0 +1,11 @@
+import buttonLogout from "../public/svg/userLogout.svg";
+import account from "../public/svg/account.svg";
+import cart from "../public/svg/cart.svg";
+
+
+export {
+    buttonLogout,
+    account,
+    cart
+}
+
