97b276556aa0a789505dd5ebe759ad7f10388bdc
Lizandro <lizandrojesus13@hotmail.com>
2024-08-11 20:31:53 +0200

    Implementando funcionalidad para saber si el producto esta o no disponible, aplicando un poco de clean code con las variables y haciendo un codigo mas legible.

--- src/app/products/ProductItem/ProductItem.jsx
+++ src/app/products/ProductItem/ProductItem.jsx
@@ -2,27 +2,40 @@ import React, { useEffect, useState } from 'react';
 import "./ProductItem.css";
 export const ProductItem = ({ product, addProductListCart }) => {
     const [isCart, setIsCart] = useState(false);
+    const handleAddToCart = () => { addProductListCart(product); setIsCart(true); }
 
-    const handleAddToCart = () => {
-        addProductListCart(product);
-        setIsCart(true);
-    }
-    useEffect(() => { }, [isCart])
+    const { imageProduct, quantity, fullname, price, description } = product;
+    const imgExist = imageProduct && imageProduct;
+    const stockProduct = quantity > 0 ? 'Disponible' : "No disponible";
+    const statusProductColor = quantity > 0 ? "status-avaliable" : "status-no-avaliable";
+    const tagNameImg = imageProduct.split("/")[8];
 
+    useEffect(() => { }, [isCart]);
     return (
 
         <div className='product-item'>
-            <img src={product.imageProduct ? ` ${product.imageProduct}` : null} />
+            <div>
+                <img src={imgExist} alt={tagNameImg} />
+            </div>
 
             <div className='product-item-info'>
-                <p>{product.fullname}</p>
-                <p>{product.price}€</p>
+                <div>
+                    <p>{fullname}</p>
+                </div>
+                <div>
+                    <p><span className={statusProductColor}></span>{stockProduct}</p>
+                    <p>{price}€</p>
+                </div>
             </div>
             <div className='product-item-description'>
-                <p>{product.description}</p>
+                <p>{description}</p>
             </div>
             <div className='product-add-cart'>
-                <p className='productAddCart' onClick={handleAddToCart}>Añadir al carrito</p>
+                <p className='productAddCart'
+                    onClick={handleAddToCart}
+                >
+                    Añadir al carrito
+                </p>
             </div>
         </div >
     )
