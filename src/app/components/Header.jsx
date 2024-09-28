4412cc6150d297f238e8412539bb19aa40800060
Lizandro <lizandrojesus13@hotmail.com>
2024-09-29 00:41:52 +0200

    Agregando iconos en la parte del header y aplicando nuevos estilos

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { FaPhoneAlt, FaShippingFast, FaClock } from 'react-icons/fa'; 
+
 export const Header = () => {
     return (
-        <div className='bg-orange-100 flex justify-between md:justify-around text-stone-600 text-lg md:text-3xl font-ligth p-5'>
-            <p>Contacto: 926123456</p>
-            <p>Envíos gratis a partir de 20€</p>
-            <p>Lunes a Viernes: 9:00 - 17:00 </p>
+        <div className='bg-gray-100 flex flex-col md:flex-row justify-between md:justify-around items-center text-stone-600 text-lg md:text-2xl font-light p-4 shadow-md'>
+            <div className='flex items-center space-x-2'>
+                <FaPhoneAlt className='text-xl' />
+                <p>Contacto: <strong>926 123 456</strong></p>
+            </div>
+            <div className='flex items-center space-x-2'>
+                <FaShippingFast className='text-xl' />
+                <p className='text-center'>Envíos gratis a partir de <strong>20€</strong></p>
+            </div>
+            <div className='flex items-center space-x-2'>
+                <FaClock className='text-xl' />
+                <p>Lunes a Viernes: <strong>9:00 - 17:00</strong></p>
+            </div>
         </div>
-    )
-}
+    );
+};
