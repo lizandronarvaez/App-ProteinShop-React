2a6d863c5f543226163b6c7abd63955d8572c80f
Lizandro <lizandrojesus13@hotmail.com>
2024-09-29 00:39:21 +0200

    Cambiando logica para mostrar o ocultar los enlaces

--- src/app/components/nav/Nav.jsx
+++ src/app/components/nav/Nav.jsx
@@ -8,21 +8,25 @@ export const Nav = () => {
     const navigate = useNavigate();
     const [toggle, setToggle] = useState(false);
     const toggleClass = () => setToggle(!toggle);
-
     return (
         <>
             <div className='w-11/12 md:w-4/5 mx-auto'>
                 <div className='py-5 mx-auto flex justify-between border-b-2 '>
                     <div className='flex items-end hover:cursor-pointer'>
                         <img className='w-20 h-20' src={LogoTienda} alt={LogoTienda} />
-                        <h2 className='text-3xl md:text-5xl font-light' onClick={() => navigate("/")}>Protein<span className='text-5xl md:text-7xl font-semibold font-sans'>Shop</span></h2>
+                        <h2 className='text-3xl md:text-5xl font-light' onClick={() => navigate("/")}>
+                            Protein
+                            <span className='text-5xl md:text-7xl font-semibold font-sans'>
+                                Shop
+                            </span>
+                        </h2>
                     </div>
                     <div className='flex justify-between items-end'>
                         <Nav_Account_Cart />
-                        <HamburgerMenu toggleClass={toggleClass} isMobile={true} />
+                        <HamburgerMenu toggleClass={toggleClass} />
                     </div>
                 </div>
-                {!toggle && <NavItems />}
+                <NavItems closeToggleItem={setToggle} toggleIsActive={toggle} />
             </div>
         </>
     )
