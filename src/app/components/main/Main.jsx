251fd631842727cdd0cf5f09a49ab269329bfb60
Lizandro <lizandrojesus13@hotmail.com>
2024-09-29 00:40:00 +0200

    Creando slider con imagenes para un carrusel

--- src/app/components/main/Main.jsx
+++ src/app/components/main/Main.jsx
@@ -3,11 +3,19 @@ import "./Main.css";
 import { Top_Sales } from './top-sales/Top_Sales';
 import { Discounts } from './discounts/Discounts';
 import { InfoStore } from './info-store/InfoStore';
+import { SliderImg } from './SliderImg';
 export const Main = () => {
+
+  const images = [
+    "../../../../public/backgrounds-main/background-main.webp",
+    "../../../../public/backgrounds-main/nutricion-deporte-800x600.jpg"
+  ]
   return (
     <>
+    <div className='py-5'>
+      <SliderImg images={images} />
+    </div>
       <div className='w-11/12 md:w-4/5 mx-auto'>
-        <div className='main-img'></div>
         <Top_Sales />
       </div>
       <div>
