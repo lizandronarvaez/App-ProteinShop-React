715b5a444fa89ff97ba5e8dc51fa2d7fe6470f61
Lizandro <lizandrojesus13@hotmail.com>
2024-09-29 00:39:38 +0200

    Logica para ocultar o mostrar los enlaces

--- src/app/components/nav/NavItems.jsx
+++ src/app/components/nav/NavItems.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-export const NavItems = () => {
+export const NavItems = ({ closeToggleItem, toggleIsActive }) => {
   const fontSemibold = "px-3 py-5 font-normal hover:font-semibold border-b-2 border-transparent hover:border-black transition-all duration-300";
+  const menuIsActive = toggleIsActive ? "" : "hidden";
+
   return (
     <>
-      <nav className='border-b-2'>
-          <div className='flex flex-col md:flex-row justify-around p-10 text-3xl font-sans'>
-            <Link className={fontSemibold} to="/products" >Productos</Link>
-            <Link className={fontSemibold} >Información</Link>
-            <Link className={fontSemibold} >Acerca</Link>
-            <Link className={fontSemibold} >Contacto</Link>
-          </div>
+      <nav className={`${menuIsActive} md:block border-b-2`}>
+        <div className={`flex flex-col md:flex-row justify-around py-5 text-3xl font-sans`}>
+          <Link className={fontSemibold} to="/products" onClick={() => closeToggleItem(false)}>Productos</Link>
+          <Link className={fontSemibold} onClick={() => closeToggleItem(false)} >Sobre Nosotros</Link>
+          <Link className={fontSemibold} onClick={() => closeToggleItem(false)} >Preguntas Frecuentes</Link>
+          <Link className={fontSemibold} onClick={() => closeToggleItem(false)}>Contacto</Link>
+        </div>
       </nav>
     </>
   )
