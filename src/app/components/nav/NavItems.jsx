02eb1ac70ef4807427c49a8b027e5accd3ce1c91
Lizandro <lizandrojesus13@hotmail.com>
2024-10-03 21:24:22 +0200

    Solucionando hover de enlaces

--- src/app/components/nav/NavItems.jsx
+++ src/app/components/nav/NavItems.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 export const NavItems = ({ closeToggleItem, toggleIsActive }) => {
-  const fontSemibold = "px-3 py-5 font-normal hover:font-semibold border-b-2 border-transparent hover:border-black transition-all duration-300";
+  const fontSemibold = "px-3 py-5 font-light mx-auto md:text-4xl border-b-2 border-transparent hover:border-orange-300 transition-all duration-300";
   const menuIsActive = toggleIsActive ? "" : "hidden";
 
   return (
     <>
-      <nav className={`${menuIsActive} md:block border-b-2`}>
+      <nav className={`${menuIsActive} md:block border-b`}>
         <div className={`flex flex-col md:flex-row justify-around py-5 text-3xl font-sans`}>
           <Link className={fontSemibold} to="/products" onClick={() => closeToggleItem(false)}>Productos</Link>
           <Link className={fontSemibold} onClick={() => closeToggleItem(false)} >Sobre Nosotros</Link>
