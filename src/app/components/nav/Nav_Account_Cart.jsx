4b9859477532ac831b301b5582831adf712d575f
Lizandro <lizandrojesus13@hotmail.com>
2024-10-03 21:23:44 +0200

    Cambiando logica al div

--- src/app/components/nav/Nav_Account_Cart.jsx
+++ src/app/components/nav/Nav_Account_Cart.jsx
@@ -18,8 +18,8 @@ export const Nav_Account_Cart = () => {
         <div>
             <div className='flex px-5 space-x-5 md:space-x-10 relative'>
                 <UserAuthenticated toggleModalAuthenticated={toggleModal} />
-                <div className='flex items-center cursor-pointer relative'>
-                    <img className='w-10 h-10'  src={cart} alt="Cart" onClick={() => setTrolleyIsOpen(true)} />
+                <div className='flex items-center cursor-pointer relative' onClick={() => setTrolleyIsOpen(true)}  >
+                    <img className='w-10 h-10'  src={cart} alt="Cart" />
                     {cartProducts.length > 0 && 
                         <p className='absolute -top-2 -right-2 md:-top-2 md:-right-1 bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center text-xl font-semibold'>
                             {cartProducts.length}
