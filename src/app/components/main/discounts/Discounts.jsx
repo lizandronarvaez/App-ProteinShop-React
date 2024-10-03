40370c47549d9133f3a11555975e16db7d29a95a
Lizandro <lizandrojesus13@hotmail.com>
2024-10-03 21:22:55 +0200

    Cambio de padding al formulario en pantallas grandes

--- src/app/components/main/discounts/Discounts.jsx
+++ src/app/components/main/discounts/Discounts.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { FormDiscounts } from './FormDiscounts/FormDiscounts'
 export const Discounts = () => {
   return (
-    <div className='w-full bg-orange-300 px-5 md:px-10 py-10 md:py-52 text-center'>
+    <div className='w-full bg-orange-300 px-5 md:px-10 py-10 md:py-20 text-center'>
       <div>
         <h3 className='text-3xl md:text-5xl font-light text-stone-900 flex flex-col'>Suscribete a nuestro boletín de noticias
           <span className=' md:text-7xl font-semibold text-stone-800'> y recibe descuentos</span>
