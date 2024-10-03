933f30e956ff6f6dc17d3454e568f4f59d910f29
Lizandro <lizandrojesus13@hotmail.com>
2024-10-03 21:23:28 +0200

    Cambiando el texto de informacion de los productos mas vendidos

--- src/app/components/main/top-sales/Top_Sales.jsx
+++ src/app/components/main/top-sales/Top_Sales.jsx
@@ -3,15 +3,17 @@ import { useSelector } from 'react-redux';
 import { ProductItem } from '../../../products/ProductItem';
 
 export const Top_Sales = () => {
-    const { productsDb } = useSelector(state => state.products)
-    const filterProductsTop = productsDb.filter(product => product.price <= 10 && product.price <= 50)
+    const { productsDb } = useSelector(state => state?.products)
+    const filterProductsTop = productsDb?.filter(product => product.price <= 10 && product.price <= 50)
 
     return (
         <div className='py-20'>
-            <h2 className='bg-orange-200 uppercase font-light text-center text-stone-600 text-3xl md:text-5xl py-10'>TOP Productos Ventas</h2>
+            <h2 className='bg-orange-200 text-center text-stone-600 text-lg md:text-3xl font-semibold py-10 mb-8'>
+                ¡No te pierdas nuestras ofertas en los productos más vendidos!
+            </h2>
             {!filterProductsTop?.length && <p className='text-center'>Cargando productos...</p>}
             <div className='mx-auto grid grid-cols-2 md:grid-cols-4 place-items-center pt-10 gap-10'>
-                {filterProductsTop.length && filterProductsTop.map(product => <ProductItem key={product.id} product={product} />)}
+                {filterProductsTop?.length && filterProductsTop?.map(product => <ProductItem key={product?.id} product={product} />)}
             </div>
         </div>
     )
