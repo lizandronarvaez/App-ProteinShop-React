4339b94f5b71f219bec8b20cb0f28e294a7ab016
Lizandro <lizandrojesus13@hotmail.com>
2024-10-08 21:47:30 +0200

    commit

--- src/app/components/main/Main.jsx
+++ src/app/components/main/Main.jsx
@@ -3,18 +3,20 @@ import { Top_Sales } from './top-sales/Top_Sales';
 import { Discounts } from './discounts/Discounts';
 import { InfoStore } from './info-store/InfoStore';
 import { SliderImg } from './SliderImg';
-import Image1 from  "../../../../public/backgrounds-main/background-main.webp";
-import Image2 from     "../../../../public/backgrounds-main/nutricion-deporte-800x600.jpg"
+import Image1 from "../../../../public/backgrounds-main/background-main.webp";
+import Image2 from "../../../../public/backgrounds-main/nutricion-deporte-800x600.jpg"
 export const Main = () => {
-  const images = [Image1,Image2]
+  const images = [Image1, Image2]
   return (
     <>
-    <div className='py-5'>
-      <SliderImg images={images} />
-    </div>
+      <div className='py-5'>
+        <SliderImg images={images} />
+      </div>
+
       <div className='w-11/12 md:w-4/5 mx-auto'>
         <Top_Sales />
       </div>
+      
       <div>
         <Discounts />
         <InfoStore />
